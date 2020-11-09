15715bfe9ab9e80a41ff978b8de9297f201980d2
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-11-09 14:30:01 -0600

    facilities werks now

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -3,6 +3,11 @@ import { useOfficers } from "../officers/OfficerProvider.js"
 import { useConvictions } from "../convictions/ConvictionProvider.js"
 import { Criminal } from "./Criminal.js"
 import { WitnessList } from "../witnesses/WitnessList.js"
+import {
+  getCriminalFacilities,
+  useCriminalFacilities,
+} from "../facilities/CriminalFacilitiesProvider.js"
+import { getFacilities, useFacilities } from "../facilities/FacilitiesProvider.js"
 
 const contentTarget = document.querySelector(".criminalsContainer")
 const eventHub = document.querySelector(".container")
@@ -10,20 +15,34 @@ const eventHub = document.querySelector(".container")
 // Listen for the custom event you dispatched in ConvictionSelect
 
 export const CriminalList = () => {
-  return getCriminals().then(() => {
-    const appStateCriminals = useCriminals()
-    render(appStateCriminals)
-  })
+  return getCriminals()
+    .then(getCriminalFacilities)
+    .then(getFacilities)
+    .then(() => {
+      const facilities = useFacilities()
+      const crimFac = useCriminalFacilities()
+      const criminals = useCriminals()
+
+      console.log("All arrayd", facilities, crimFac, criminals)
+      render(criminals, facilities, crimFac)
+    })
 }
 
-const render = (criminalCollection) => {
-  contentTarget.innerHTML = `
-  <h3 class="criminals--title">Glassdale's Most Wantedest<h3>
-  <div class="criminalList">
-  ${criminalCollection.map((criminal) => Criminal(criminal)).join(" ")}
-  </div>
-  <div class="alibis"></div>
-  `
+const render = (criminalCollection, allFacilities, allRelationships) => {
+  contentTarget.innerHTML = criminalCollection
+    .map((criminalObj) => {
+      const facilityRelationshipForThisCriminal = allRelationships.filter(
+        (cf) => cf.criminalId === criminalObj.id
+      )
+      const facilities = facilityRelationshipForThisCriminal.map((cf) => {
+        const matchingFacilityObject = allFacilities.find(
+          (facility) => facility.id === cf.facilityId
+        )
+        return matchingFacilityObject
+      })
+      return Criminal(criminalObj, facilities)
+    })
+    .join("")
 }
 
 // Chooses the criminals by crime using the dropdown
@@ -67,3 +86,10 @@ eventHub.addEventListener("officerChosen", (event) => {
 eventHub.addEventListener("displayWitnesses", (event) => {
   WitnessList()
 })
+
+// {
+/* <h3 class="criminals--title">Glassdale's Most Wantedest<h3>
+  <div class="criminalList"> */
+//   </div>
+//   <div class="alibis"></div>
+// }
